a23e9bbb68d0949c847e4a952add10d54ae3d7f8
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-25 19:05:37 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align='center'>
-            <Box mr='4' textAlign='right'>
-                <Text>Gabriel Feitosa</Text>
-                <Text color='gray.300' fontSize='small'>
-                    contato.gabrielfeitosa@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr='4' textAlign='right'>
+                    <Text>Gabriel Feitosa</Text>
+                    <Text color='gray.300' fontSize='small'>
+                        contato.gabrielfeitosa@gmail.com
+                    </Text>
+                </Box>
+            )}
             <Avatar size='md' name='Gabriel Feitosa' src='https://github.com/br-gabriel.png' />
         </Flex>
     );
