132b23c4b90e362f78236a4a0d4a5d6e4b4067f8
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-25 21:00:01 -0300

    responsividade concluida

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,18 +1,23 @@
-import { Box, Button, Text, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Box, Button, Text, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header/Index";
-import { Pagination } from "../../components/Pagination/index";
+import { Pagination } from "../../components/Pagination/Index";
 import { Sidebar } from "../../components/Sidebar/Index";
 
 export default function UserList() {
-    return (
+      const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+      });
+
+      return (
         <Box>
           <Header />
 
           <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
             <Sidebar />
 
-            <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+            <Box flex='1' borderRadius={8} bg='gray.800' p='8' w='90%'>
               <Flex mb='8' justify='space-between' alignItems='center'>
                 <Heading size='lg' fontWeight='normal'>Usuários</Heading>
                 
@@ -24,17 +29,17 @@ export default function UserList() {
               <Table colorScheme='whiteAlpha'>
                   <Thead>
                       <Tr>
-                          <Th px='6' color='gray.300' w='8'>
+                          <Th px={['4', '4', '6']} color='gray.300' w='8'>
                               <Checkbox colorScheme='pink' />
                           </Th>
                           <Th>Usuário</Th>
-                          <Th>Data de cadastro</Th>
+                          { isWideVersion && <Th>Data de cadastro</Th> }
                           <Th w='8'></Th>
                       </Tr>
                   </Thead>
                   <Tbody>
                       <Tr>
-                          <Td px='6'>
+                          <Td px={['4', '4', '6']}>
                             <Checkbox colorScheme='pink' />
                           </Td>
                           <Td>
@@ -43,12 +48,7 @@ export default function UserList() {
                                   <Text fontSize='sm' color='gray.300'>contato.gabrielfeitosa@gmail.com</Text>
                               </Box>
                           </Td>
-                          <Td>04 de Abril, 2022</Td>
-                          <Td>
-                            <Button as='a' size='sm' fontSize='sm' colorScheme='blue' leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}>
-                                Editar
-                            </Button>
-                          </Td>
+                          { isWideVersion && <Td>04 de Abril, 2022</Td> }
                       </Tr>
                   </Tbody>
               </Table>
