78c74fd7ac7e824fbfab3ccd645b7ff6af295fc9
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-28 16:40:02 -0300

    ssr com react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,14 +4,17 @@ import { Header } from "../../components/Header/Index";
 import { Pagination } from "../../components/Pagination/Index";
 import { Sidebar } from "../../components/Sidebar/Index";
 import NextLink from "next/link";
-import { useUsers } from "../../services/hooks/useUser";
+import { getUsers, useUsers } from "../../services/hooks/useUser";
 import { useState } from "react";
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
-  const { data, isLoading, isFetching, error } = useUsers(page)
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  })
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -101,4 +104,14 @@ export default function UserList() {
       </Flex>
     </Box>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1)
+
+  return {
+    props: {
+      users,
+    }
+  }
 }
