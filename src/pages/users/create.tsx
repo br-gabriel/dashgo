4f1090c4169edd30da049fabb65a2c061294d479
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-28 16:33:23 -0300

    mutatuions response

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header/Index";
 import { Sidebar } from "../../components/Sidebar/Index";
+import { useMutation } from 'react-query';
 
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
 import { SubmitHandler, useForm } from  'react-hook-form';
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -23,7 +27,23 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
-  //Atualizar isso aqui
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        }
+      })
+
+      return response.data.user;
+    }, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users')
+      }
+    })
+
     const { 
       register,
       formState: {errors, isSubmitting},
@@ -33,8 +53,8 @@ export default function CreateUser() {
     })
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      console.log(values)
+      await createUser.mutateAsync(values);
+      router.push('/users')
     }
 
     return (
@@ -58,7 +78,7 @@ export default function CreateUser() {
 
               <VStack spacing='8'>
                 <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
-                    <Input name='name' label='Nome completo' error={errors.name}  {...register('name')}/>
+                    <Input name='name' type='text' label='Nome completo' error={errors.name}  {...register('name')}/>
                     <Input name='email' type='email' label='E-mail' error={errors.email}   {...register('email')}/>
                 </SimpleGrid>
 
