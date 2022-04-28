7b32b90417f0fdc5cbf7f2ea6da60fb5836c2eb3
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-04-27 22:49:53 -0300

    hook de listagem de usuários criado

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,34 +1,13 @@
 import { Box, Button, Text, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header/Index";
 import { Pagination } from "../../components/Pagination/Index";
 import { Sidebar } from "../../components/Sidebar/Index";
-import { useQuery } from 'react-query';
 import Link from "next/link";
+import { useUsers } from "../../services/hooks/useUser";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString(
-          'pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          }
-        )
-      }
-    });
-
-    return users;
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -44,7 +23,10 @@ export default function UserList() {
 
         <Box flex='1' borderRadius={8} bg='gray.800' p='8' w='90%'>
           <Flex mb='8' justify='space-between' alignItems='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+              {!isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
+            </Heading>
 
             <Link href='/users/create' passHref>
               <Button as='a' size='sm' fontSize='sm' colorScheme='pink' leftIcon={<Icon as={RiAddLine} fontSize='20' />}>
